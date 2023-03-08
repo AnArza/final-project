454452f2565cf4d5e7519de486fc4b1c45646df2
agnes <agnesmuradyan@gmail.com>
2023-03-09 00:07:42 +0400

    uff

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -80,7 +80,7 @@ environ.Env.read_env()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': os.environ.get('DATABASE_NAME'),
+        "NAME": os.environ.get("DATABASE_NAME"),
         "USER": os.environ.get("USER_NAME"),
         "PASSWORD": os.environ.get("PASSWORD"),
         "HOST": os.environ.get("HOST"),
